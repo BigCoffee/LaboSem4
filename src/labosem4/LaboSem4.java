fb5be2ebfb7f93b62fddeb11a8033a618a14c402
cg791291 <cg791291@C31618.ens.uqam.ca>
2014-01-30 23:22:04 -0300

    FirstCommit Test

--- src/labosem4/LaboSem4.java
+++ src/labosem4/LaboSem4.java
@@ -16,7 +16,7 @@ public class LaboSem4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int patate = 3;
     }
     
 }
